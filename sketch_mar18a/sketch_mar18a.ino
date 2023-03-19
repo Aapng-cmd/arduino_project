55d2b45a15f40fd7a8d986190801528d8b4a75fa
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2023-03-19 15:39:55 +0300

    Update sketch_mar18a.ino

--- sketch_mar18a/sketch_mar18a.ino
+++ sketch_mar18a/sketch_mar18a.ino
@@ -1,4 +1,4 @@
-/* Кунина Лиза, Варежников Кирилл, камонда 3, "Умная кормушка" */
+/* Кунина Лиза, команда 3, "Таймер" */
 #include <Wire.h>
 
 #include <LiquidCrystal_I2C.h>
@@ -6,176 +6,85 @@
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4); // Устанавливаем дисплей
 
-int val = 0; // Для считывания показаний потенциометра
-#define potpin A0 // Подключение потенциометра
-#define buttonPin1 A2// Подключение кнопки
-#define buttonPin2 6
-int flag = 0; // для кнопки
-unsigned int counter = 0;
+#define PIP 8
+#define pot A0
 
-int SEC = 0;
-
-unsigned long timer = 0;
-int tam;
-
-unsigned int disp_func(unsigned int counter, bool boot = false)
+int timm(int q = 5)
 {
-  int button1 = !digitalRead(buttonPin1);
-  int val1 = 0;
-  if (!boot)
+  unsigned long tim = 0;
+  unsigned long timer = millis();
+  int o = 0;
+  q *= 1000;
+
+  while (millis() - timer < q)
   {
-    if (counter == 1)
+    String s1 = "Time left: ";
+    String s2 = "Time chosen: ";
+    o = q / 1000 - (millis() - timer) / 1000;
+    s1 += o;
+    s1 += "  ";
+    s2 += q / 1000;
+    s2 += "  ";
+    lcd.print(s1);
+    lcd.setCursor(0, 1);
+    lcd.print(s2);
+    lcd.setCursor(0, 0);
+  }
+  
+  lcd.clear();
+  lcd.print("Done!");
+  lcd.setCursor(0, 0);
+
+  for (int i = 0; i < 7; i++)
+  {
+    tim = millis();   
+    while (millis() - tim < 1000)
     {
-      
-    }
-    
-    else if (counter == 2)
-    {
-      
+      if (i % 2)
+      {
+        tone(PIP, 2000);
+      }
+      else
+      {
+        noTone(PIP);
+      }
     }
   }
-  else
-  {   
-    while (!button1)
-    {
-      String s = "Choose a time >> ";
-      val1 = map(analogRead(potpin), 0, 1023, 0, 60);
-      s += val1;
-      s += "  ";
-      // lcd.print(s.concat(val1));
-      lcd.print(s);
-      // lcd.setCursor(17, 0);
-      // lcd.print(val1);
-      // lcd.clear();
-      // lcd.setCursor(0, 0);
-    }
-      
-    return val1;
-  }
+  return 0;
 }
 
 void setup()
 {
-  // pinMode(buttonPin1, INPUT_PULLUP);
-  pinMode(buttonPin1, 0);
-  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(pot, 0);
   lcd.init();
   lcd.backlight(); // Включаем подсветку дисплея
-  lcd.print("Ready to start!");
-
-  timer = millis();
+  lcd.clear();
+  lcd.setCursor(0, 0);
 }
-
-int boot_l = 0;
-unsigned long time_left = 0;
 
 void loop()
 {
-  int button1 = !digitalRead(buttonPin1);
-  int button2 = !digitalRead(buttonPin2);
-  int start_up = 0;
-
-  val = analogRead(potpin);
-  val = map(val, 0, 1023, 0, 60);
-
   unsigned long tim = millis();
-  while (millis() - tim > 1000)
-  {
-    if (button1 == 1)
-    {
-      counter++;
-    }
-  }
-  
-  // counter = 1;
+  int val = 5;
+  int o = 0;
 
-  if (counter > 0 && !boot_l)
+  while (millis() - tim < 4 * 1000)
   {
-    time_left = disp_func(0, true);
-    boot_l = 1;
-    start_up = 1;
-  }
-  else
-  {
-    disp_func(counter);
-  }
+    String s1 = "Chosen time: ";
+    String s2 = "WARNING!  ";
+    val = map(analogRead(pot), 0, 1023, 3, 20);
+    s1 += val;
+    s1 += "  ";
+    o = 4 - (millis() - tim) / 1000 - 1;
+    s2 += o;
 
-  while (time_left > 0)
-  {
-    lcd.setCursor(0, 0);
-    lcd.print("Time left: " + time_left);
-    // lcd.clear();
-  }
-
-  if (start_up)
-  {
-
-  }
-
-  /*
-  if (button1 == 1)
-  {
+    lcd.print(s1);
     lcd.setCursor(0, 1);
-    lcd.print("Time:");
-    lcd.setCursor(8, 1);
-    lcd.print(val);
+    lcd.print(s2);
+    lcd.setCursor(0, 0);
   }
-  */
+
+  lcd.clear();
+  timm(val); 
 
 }
-
-
-/* val = analogRead(potpin);
-     if (button1 && flag && millis() - btnTimer < 1000)
-  val = map(val, 0, 1023, 0, 60);
-  if (button1 && flag && millis() - btnTimer > 2000)
-  {
-   btnTimer = millis();
-
-  lcd.print("Time:");
-  lcd.setCursor(8, 1);
-  lcd.print(val);
-  delay(300);
-*/
-
-
-/* // читаем инвертированное значение для удобства
-  bool btnState = !digitalRead(3);
-  if (btnState && !flag && millis() - btnTimer > 100) {
-   flag = true;
-   btnTimer = millis();
-   Serial.println("press");
-  }
-  if (btnState && flag && millis() - btnTimer > 500) {
-   btnTimer = millis();
-   Serial.println("press hold");
-  }
-  if (!btnState && flag && millis() - btnTimer > 500) {
-   flag = false;
-   btnTimer = millis();
-   //Serial.println("release");
-  }
-*/
-
-/*int SEC = 0;
-  int MIN = 0;
-  unsigned long timer;
-
-  void setup() {
-  Serial.begin(9600);
-  timer = millis();
-  }
-
-  void loop() {
-  // если прошло 1000 мс - прибавляем одну секунду к переменной SEC
-  if (millis() - timer > 1000) {
-    timer = millis();
-    SEC = SEC + 1;
-    if (SEC > 59) { SEC = 0; MIN = MIN + 1; }
-    // выводим текущий счетчик времени на монитор порта
-    Serial.println(String(MIN) + " : " + String(SEC));
-  }
-
-  }
-
-*/
